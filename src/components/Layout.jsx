6d40a04a1fcd4aae48d7996f43580e4d01b558bf
Soohanur <soohanur@gmail.com>
2026-05-13 23:52:23 +0600

    Revert "feat(animation): sitewide GSAP reveal for every heading and image"
    This reverts commit 76ce60d58b59647d608abe45de92f680ece2437d.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,9 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import { useSmoothScroll } from "@/lib/useSmoothScroll";
-import { useGlobalReveal } from "@/lib/useGlobalReveal";
 
 export default function Layout() {
   useSmoothScroll();
-  useGlobalReveal();
   return (
     <div className="bg-background text-foreground min-h-screen">
       <Header />
